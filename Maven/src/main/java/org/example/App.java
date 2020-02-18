ba069e26853c1a8d5ef4d1a666ca2f92772764e2
yusrahsoeker <40632654+yusrahsoeker@users.noreply.github.com>
2020-02-18 20:48:13 +0200

    Update App.java

--- Maven/src/main/java/org/example/App.java
+++ Maven/src/main/java/org/example/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args ) {
         System.out.println( "Hello World! \n I created a branch and this is my first edit" );
-        System.out.println( "First edit thingz!");
+        System.out.println( "First edit things!" );
     }
 }
